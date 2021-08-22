6d87382587ad4931fbcd85c91f949c7938a35dc5
Arsenii Liunsha <me@arslee.dev>
2021-08-22 19:53:22 +0900

    chore(pay): Rework responses

--- routes/economy.py
+++ routes/economy.py
@@ -14,18 +14,31 @@ def pay():
         amount = int(args['amount'])
 
         payer_obj = db.collection('users').document(payer).get()
+        payer_balance = payer_obj.to_dict()['balance']
+
+        if payer_balance < amount:
+            return {
+                "message": "Недостаточно средств."
+            }, 403
 
         try:
             payee_obj = db.collection('users').where(
                 'name', '==', payee).get()[0]
         except IndexError:
-            return 'user-not-found'
+            return {
+                'message': 'Пользователь не найден.'
+            }, 404
 
-        payer_obj.reference.update(
-            {'balance': payer_obj.to_dict()['balance'] - amount})
-        payee_obj.reference.update(
-            {'balance': payee_obj.to_dict()['balance'] + amount})
+        payee_balance = payee_obj.to_dict()['balance']
 
-        return 'ok'
+        payer_obj.reference.update({'balance': payer_balance - amount})
+        payee_obj.reference.update({'balance': payee_balance + amount})
+
+        return {
+            'message': f'Успешно переведено ${amount} ИБ пользователю ${payee}!'
+        }, 200
+
     except Exception as e:
-        return str(e)
+        return {
+            'message': 'Внутренняя ошибка сервера: ' + str(e)
+        }, 500
