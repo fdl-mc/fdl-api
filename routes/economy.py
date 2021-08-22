3d5551f5fdcdd9cd0b114eee2a6f8b2e1eff473a
Arsenii Liunsha <me@arslee.dev>
2021-08-22 20:01:40 +0900

    chore(pay): Refactor and format

--- routes/economy.py
+++ routes/economy.py
@@ -13,32 +13,24 @@ def pay():
         payee = args['payee']
         amount = int(args['amount'])
 
-        payer_obj = db.collection('users').document(payer).get()
-        payer_balance = payer_obj.to_dict()['balance']
+        payer_doc = db.collection('users').document(payer).get()
+        payer_balance = payer_doc.to_dict()['balance']
 
         if payer_balance < amount:
-            return {
-                "message": "Недостаточно средств."
-            }, 403
+            return {'message': 'Недостаточно средств.'}, 403
 
-        try:
-            payee_obj = db.collection('users').where(
-                'name', '==', payee).get()[0]
-        except IndexError:
-            return {
-                'message': 'Пользователь не найден.'
-            }, 404
+        query = db.collection('users').where('name', '==', payee).get()
 
-        payee_balance = payee_obj.to_dict()['balance']
+        if (len(query) == 0):
+            return {'message': 'Пользователь не найден.'}, 404
 
-        payer_obj.reference.update({'balance': payer_balance - amount})
-        payee_obj.reference.update({'balance': payee_balance + amount})
+        payee_doc = query[0]
+        payee_balance = payee_doc.to_dict()['balance']
 
-        return {
-            'message': f'Успешно переведено ${amount} ИБ пользователю ${payee}!'
-        }, 200
+        payer_doc.reference.update({'balance': payer_balance - amount})
+        payee_doc.reference.update({'balance': payee_balance + amount})
+
+        return {'message': f'Успешно переведено ${amount} ИБ пользователю ${payee}.'}, 200
 
     except Exception as e:
-        return {
-            'message': f'Внутренняя ошибка сервера: {str(e)}'
-        }, 500
+        return {'message': f'Внутренняя ошибка сервера: {str(e)}'}, 500
