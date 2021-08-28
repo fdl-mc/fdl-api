52eb32b5f2d406b6ed263e644b0709472d1894bb
Arsenii Liunsha <me@arslee.dev>
2021-08-28 13:56:57 +0900

    feat: Add stats endpoints

--- lib/src/routes/stats.dart
+++ lib/src/routes/stats.dart
@@ -1,9 +1,60 @@
+import 'package:dart_minecraft/dart_minecraft.dart';
 import 'package:fdl_server/src/interfaces/controller.dart';
+import 'package:shelf/shelf.dart';
 import 'package:shelf_router/shelf_router.dart';
 
 class StatsController extends Controller {
   @override
   Router get router {
-    return Router();
+    final router = Router();
+
+    router.get('/main', _mainStats);
+    router.get('/creative', _creativeStats);
+
+    return router;
+  }
+
+  Future<Response> _mainStats(Request request) async {
+    final server = await ping(
+      'play.fdl-mc.ru',
+      port: 25565,
+      timeout: Duration(seconds: 5),
+    );
+    final response = server!.response!;
+
+    return Response.ok({
+      'ip': 'play.fdl-mc.ru',
+      'port': 25565,
+      'description': response.description.description,
+      'version': response.version.name.split(' ').last,
+      'latency': server.ping,
+      'players': {
+        'online': response.players.online,
+        'max': response.players.max,
+        'names': response.players.sample.map((e) => e.name).toList(),
+      }
+    }.toString());
+  }
+
+  Future<Response> _creativeStats(Request request) async {
+    final server = await ping(
+      'creative.fdl-mc.ru',
+      port: 25565,
+      timeout: Duration(seconds: 5),
+    );
+    final response = server!.response!;
+
+    return Response.ok({
+      'ip': 'creative.fdl-mc.ru',
+      'port': 25565,
+      'description': response.description.description,
+      'version': response.version.name.split(' ').last,
+      'latency': server.ping,
+      'players': {
+        'online': response.players.online,
+        'max': response.players.max,
+        'names': response.players.sample.map((e) => e.name).toList(),
+      }
+    }.toString());
   }
 }
