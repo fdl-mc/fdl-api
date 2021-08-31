e8fda449009f128296f6e4f5d5896c707481a176
Arsenii Liunsha <me@arslee.dev>
2021-08-31 11:03:49 +0900

    feat: Add POST args check middleware

---
+++ lib/src/middlewares/post_args_check.dart
@@ -0,0 +1,32 @@
+import 'package:fdl_server/src/interfaces/middleware.dart';
+import 'package:shelf/shelf.dart';
+
+/// Checks POST args
+class PostArgsMiddleware extends IMiddleware {
+  final List<String> requiredArgs;
+
+  PostArgsMiddleware({required this.requiredArgs});
+
+  @override
+  Middleware middleware() {
+    return (innerHandler) {
+      return (request) async {
+        final args = request.context['body'] as Map<String, String>;
+
+        for (var i = 0; i < requiredArgs.length; i++) {
+          if (!args.containsKey(requiredArgs[i])) {
+            return Response(
+              400,
+              body: {
+                'errorCode': 400,
+                'errorMsg': 'Необходим аргумент ${requiredArgs[i]}',
+              }.toString(),
+            );
+          }
+        }
+
+        return innerHandler(request);
+      };
+    };
+  }
+}
