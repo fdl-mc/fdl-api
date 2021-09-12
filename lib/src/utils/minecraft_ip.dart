82eef53e87e33bf8cbd6a0881e535accc32c3de7
Arsenii Liunsha <me@arslee.dev>
2021-09-12 14:19:42 +0900

    feat(utils): Add MinecraftIp utility class

---
+++ lib/src/utils/minecraft_ip.dart
@@ -0,0 +1,18 @@
+/// A parsed Minecraft server IP.
+class MinecraftIp {
+  /// Minecraft server hostname
+  final String host;
+
+  /// Minecraft server port.
+  final int port;
+
+  MinecraftIp({
+    required this.host,
+    required this.port,
+  });
+
+  /// Parses from given ip string, e.g. `example.com:25565`.
+  MinecraftIp.parse(String ip)
+      : host = ip.split(':')[0],
+        port = int.parse(ip.split(':')[1]);
+}
