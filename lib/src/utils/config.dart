16fea674c649bc2ebe7b862662d0ca43b5972c11
Arsenii Liunsha <me@arslee.dev>
2021-09-12 13:30:57 +0900

    chore(Config): Add main and creative server ips into environment vars

--- lib/src/utils/config.dart
+++ lib/src/utils/config.dart
@@ -12,20 +12,38 @@ class Config {
   /// Server port.
   final int port;
 
+  /// Main MC server ip, e.g. example.com:25565
+  final Uri mainServerIp;
+
+  /// Creative MC server ip, e.g. example.com:25565
+  final Uri creativeServerIp;
+
   Config({
     required this.mongodbUrl,
     required this.port,
     required this.firebaseAdminCredentials,
+    required this.mainServerIp,
+    required this.creativeServerIp,
   });
 
   /// Parse environment variables.
-  /// - FDL_SERVER_MONGODB_URL for [mongodbUrl]
-  /// - FDL_SERVER_PORT for [port]
-  /// - FDL_SERVER_FBA_CREDENTIALS for [firebaseAdminCredentials]
+  /// - `FDL_SERVER_MONGODB_URL` for [mongodbUrl]
+  /// - `FDL_SERVER_PORT for` [port]
+  /// - `FDL_SERVER_FBA_CREDENTIALS` for [firebaseAdminCredentials]
+  /// - `FDL_SERVER_MAIN_IP` for [mainServerIp]
+  /// - `FDL_SERVER_CREATIVE_IP` for [creativeServerIp]
   Config.fromEnviroment()
       : mongodbUrl = Platform.environment['FDL_SERVER_MONGODB_URL']!,
-        port = int.tryParse(Platform.environment['FDL_SERVER_PORT']!)!,
+        port = int.tryParse(
+          Platform.environment['FDL_SERVER_PORT']!,
+        )!,
         firebaseAdminCredentials = jsonDecode(
           Platform.environment['FDL_SERVER_FBA_CREDENTIALS']!,
+        ),
+        mainServerIp = Uri.parse(
+          Platform.environment['FDL_SERVER_MAIN_IP']!,
+        ),
+        creativeServerIp = Uri.parse(
+          Platform.environment['FDL_SERVER_CREATIVE_IP']!,
         );
 }
