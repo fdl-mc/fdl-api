f9554402e1cd544a59d74fff3edc4a79302839e4
Arsenii Liunsha <me@arslee.dev>
2021-09-12 11:26:53 +0900

    feat: Add server stats builder

---
+++ lib/src/builders/server_stats.dart
@@ -0,0 +1,43 @@
+import 'package:fdl_server/src/interfaces/builder.dart';
+
+class ServerStatsBuilder implements IBuilder<String> {
+  String? ip;
+  int? port;
+  String? description;
+  String? version;
+  int? latency;
+  PlayersStatsBuilder? players;
+
+  @override
+  String build() {
+    return {
+      'ip': ip!,
+      'port': port!,
+      'description': description!,
+      'version': version!,
+      'latency': latency!,
+      'players': players!.build(),
+    }.toString();
+  }
+}
+
+class PlayersStatsBuilder implements IBuilder<Map<String, dynamic>> {
+  int? online;
+  int? max;
+  List<String>? names;
+
+  PlayersStatsBuilder({
+    this.online,
+    this.max,
+    this.names,
+  });
+
+  @override
+  Map<String, dynamic> build() {
+    return {
+      'online': online!,
+      'max': max!,
+      'names': names!,
+    };
+  }
+}
