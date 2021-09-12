9657dcd36a52491f64adf395815f3a25656a1235
Arsenii Liunsha <me@arslee.dev>
2021-09-12 10:56:24 +0900

    feat: Add error message builder class

---
+++ lib/src/builders/error.dart
@@ -0,0 +1,22 @@
+import 'package:fdl_server/src/interfaces/builder.dart';
+
+class ErrorMessageBuilder extends IBuilder<String> {
+  int? errorCode;
+  String? errorStatus;
+  String? errorMessage;
+
+  ErrorMessageBuilder({
+    this.errorCode,
+    this.errorStatus,
+    this.errorMessage,
+  });
+
+  @override
+  String build() {
+    return {
+      'errorCode': errorCode!,
+      'errorStatus': errorStatus!,
+      'errorMessage': errorMessage!,
+    }.toString();
+  }
+}
