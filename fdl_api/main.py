9edafd8b401f3e157b7b8300464040902938f82f
Arsenii Liunsha <me@arslee.dev>
2021-11-04 21:21:51 +0900

    chore: Add CORS middleware

--- fdl_api/main.py
+++ fdl_api/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
 from fdl_api.routes import stats, users, economy
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(stats.router)
 app.include_router(users.router)
